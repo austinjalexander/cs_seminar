0829a668884c7502c1f97bb182dcf8cd3b70a45f
austinja <austinja@bu.edu>
2015-06-19 03:36:09 +0000

    python server added, need to make class examples

---
+++ python/class5/app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html', name="Austin")
+    
+app.run(host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', 8080))
