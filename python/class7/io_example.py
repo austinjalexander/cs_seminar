96984495674c36e2d1a784577f69fe7ee987204e
austinja <austinja@bu.edu>
2015-06-24 16:58:20 +0000

    class 8 python

--- python/class7/io_example.py
+++ python/class7/io_example.py
@@ -1,19 +1,18 @@
-with open('data.txt', 'r') as f:
-    data = f.read()
-    print data
+#with open('data.txt', 'r') as f:
+#    data = f.read()
+#    print f.read()
+ 
+#with open('data.txt', 'w') as f:
+#    f.write('mo stuff')
 
-'''    
-with open('data.txt', 'w') as f:
-    f.write('some interesting data')
-    
-with open('data.txt', 'r') as f:
-    data = f.read()
-    print data
-    
-with open('data.txt', 'a') as f:
-    f.write("\nadditional data")
-    
+#with open('data.txt', 'a') as f:
+#    f.write("\nadditional data")
+
+# this is a block that reads and writes  
 with open('data.txt', 'r+') as f:
     data = f.read()
     print data
-'''
+    f.write('\nr+ rox')
+    data = f.read()
+    print data
+
--- python/class8/app.py
+++ python/class8/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
+    
     return render_template('index.html', name="Austin")
     
 app.run(host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', 8080))
